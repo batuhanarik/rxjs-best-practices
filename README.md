de84c7e425a9684255df0a0c03ad2ac201ad47b1
batuhanarik <71034853+batuhanarik@users.noreply.github.com>
2023-02-07 18:12:39 +0300

    filter & debounceTime operators

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# https-github.com-batuhanarik-rxjs-best-practices
+RxJS Operators
+#Creation Operators
+-of operator : herhangi bir nesneden observable nesne döner.
+-timer operator : belirtilen zaman sonra belirtilen zaman aralıklarında verilen işi yapar
+-from operator : of'a benziyor. herhangi bir nesneden observable döner.
+-fromEvent : bir nesneye event atayabilmemize yarar.
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-pmrgr3)
+#Filtering Operators
+-filter operator : verilen dizide belirtilen şartı sağlayan nesneleri döner.
+-debounceTime operator : gelen datalar arasındaki süre farkı verilen süreden fazlaysa döndürür.
--- index.ts
+++ index.ts
@@ -1,62 +1,82 @@
 import './style.css';
 import './index.html';
-import { of, from, map, Observable, timer } from 'rxjs';
+import { of, from, Observable, timer, filter, fromEvent } from 'rxjs';
+import { map, debounceTime } from 'rxjs/operators';
 
-// of operator
-const values = of('Batu', [1, 2, 3], 4, 4.3, 'best-practices', {
-  a: 1,
-  b: 2,
-  c: 3,
-});
+// // of operator
+// const values = of('Batu', [1, 2, 3], 4, 4.3, 'best-practices', {
+//   a: 1,
+//   b: 2,
+//   c: 3,
+// });
 
-values.subscribe((data) => {
-  console.log(data);
-});
+// values.subscribe((data) => {
+//   console.log(data);
+// });
 
-// timer operator
-const publisher = timer(2000);
-publisher.subscribe(
-  (val) => {
-    console.log('Bu yazı 2sn sonra gözükecek.');
-  },
-  (err) => {},
-  () => {
-    console.log('Veri alma işlemi tamamlandı.');
-  }
-);
+// // timer operator
+// const publisher = timer(2000);
+// publisher.subscribe(
+//   (val) => {
+//     console.log('Bu yazı 2sn sonra gözükecek.');
+//   },
+//   (err) => {},
+//   () => {
+//     console.log('Veri alma işlemi tamamlandı.');
+//   }
+// );
 
-const publisher2 = timer(4000, 2000);
-const subscription = publisher2.subscribe(
-  (val) => {
-    console.log('Bu yazı 5sn sonra her 2snde bir kez gözükecek.' + val);
-  },
-  (err) => {},
-  () => {
-    console.log('Veri alma işlemi tamamlandı.');
-  }
-); 
-setTimeout(() => {
-  console.log('Timer 2 durduruldu.');
-  subscription.unsubscribe();
-}, 10000);
+// const publisher2 = timer(4000, 2000);
+// const subscription = publisher2.subscribe(
+//   (val) => {
+//     console.log('Bu yazı 5sn sonra her 2snde bir kez gözükecek.' + val);
+//   },
+//   (err) => {},
+//   () => {
+//     console.log('Veri alma işlemi tamamlandı.');
+//   }
+// );
+// setTimeout(() => {
+//   console.log('Timer 2 durduruldu.');
+//   subscription.unsubscribe();
+// }, 10000);
 
-function stop() {
-  console.log('timer subscription durduruldu');
-  subscription.unsubscribe();
-}
+// function stop() {
+//   console.log('timer subscription durduruldu');
+//   subscription.unsubscribe();
+// }
 
-const map = new Map();
-map.set(1, 'Kitaplar');
-map.set(2, 'Kalemler');
-map.set(3, 'Silgiler');
+// const map = new Map();
+// map.set(1, 'Kitaplar');
+// map.set(2, 'Kalemler');
+// map.set(3, 'Silgiler');
 
-const fromPublisher = from(map);
-fromPublisher.subscribe(
-  (val) => {
-    console.log(`${val[0]} - ${val[1]}`);
-  },
-  (err) => {},
-  () => {
-    console.log('Veri alma işlemi tamamlandı.');
-  }
-);
+// const fromPublisher = from(map);
+// fromPublisher.subscribe(
+//   (val) => {
+//     console.log(`${val[0]} - ${val[1]}`);
+//   },
+//   (err) => {},
+//   () => {
+//     console.log('Veri alma işlemi tamamlandı.');
+//   }
+// );
+
+//filter operator
+// const myArray = from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+// myArray.pipe(filter((val) => val % 2 !== 0)).subscribe(
+//   (res) => {
+//     console.log(res);
+//   },
+//   (err) => {
+//     console.log(err);
+//   }
+// );
+
+//debounceTime operator
+const input = document.getElementById('textSearch');
+fromEvent(input, 'keyup')
+  .pipe(debounceTime(300))
+  .subscribe((data) => {
+    console.log(data.target.value);
+  });
